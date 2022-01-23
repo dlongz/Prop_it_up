d6e91dbde606e565daa6334ca5beda333a527e1f
DLongz <mrlewis1188@gmail.com>
2022-01-23 02:07:50 -0500

    complete Prop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import PersonCard from './Components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard 
+        firstName={ 'Dlongz' }
+        lastName={ "Louis"}
+        age={ 33 }
+        hColor={ 'Brown' }
+
+      />
+      <PersonCard 
+        firstName={ 'Hero' }
+        lastName={ "Louis"}
+        age={ 27 }
+        hColor={ 'Black' }
+
+      />
+      <PersonCard 
+        firstName={ 'StrongArm' }
+        lastName={ "Louis"}
+        age={ 26 }
+        hColor={ 'Black' }
+
+      />
+      <PersonCard 
+        firstName={ 'KinkyKurlz' }
+        lastName={ "Louis"}
+        age={ 18 }
+        hColor={ 'Pink' }
+
+      />
     </div>
   );
 }
